9007b52906fdf1bb393af227167060faeaf99b2f
Odetunde Olumide Claudius <80943998+odetundeolumide@users.noreply.github.com>
2022-06-16 00:23:51 +0100

    Regroup Sound files

--- DataAnnotationForm.py
+++ DataAnnotationForm.py
@@ -1,5 +1,4 @@
 # import necessary modules
-from email.policy import strict
 import os
 import sys
 import tkinter
@@ -22,16 +21,11 @@ emotion = None #widget
 emotion_rating = None #widget
 sound_list  = None #list of sound to be played per group selection
 current_selection = None  #sound group selection from dropdown menu
-file_path_dict = {"Animal Sound Block 1":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Animal/Block 1",  
-				  "Animal Sound Block 2":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Animal/Block 2",
-				  "Nature Sound Block 1":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Nature/Block 1", 
-				  "Nature Sound Block 2":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Nature/Block 2",
-				  "Nature Sound Block 3":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Nature/Block 3",  
-				  "People Sound":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/People/Block 1",
-				  "Transport Sound Block 1":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Transport/Block 1",
-				  "Transport Sound Block 2":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Transport/Block 2",
+file_path_dict = {"Animal Sound":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Animal",  
+				  "Nature Sound":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Nature", 
+				  "People Sound":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/People",
+				  "Transport Sound":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Transport",
 				  "Practice Sound":"./IADS-E/IADS-E sound stimuli (IADS-2 is not included)/Practice sound"}
-
 
 #create a function that retrieves the sound group&blocks from dropdown menu and returns the file_path using globally defined _file path dictionary
 def compile_sound_group(folder):
@@ -140,7 +134,7 @@ def play():
 	global sound_list
 	global idx
 	start_time = get_start_time() #get starttime upon clicking plays sound
-	wave_obj = sa.WaveObject.from_wave_file(path + "/" + sound_list[idx]) #input combine path of soundfile
+	wave_obj = sa.WaveObject.from_wave_file(path + "/" + sound_list[idx]) #input combined path of soundfile
 	play_obj = wave_obj.play() #play soundfile
 	play_obj.wait_done()
 
@@ -165,6 +159,8 @@ if __name__ == "__main__":
 	user_id.grid(row=0, column=0,)
 	user_id_field.grid(row=0,column=1,ipadx="30")
 	place_holder = Label(root,text="").grid(row=1,column=0)
+	place_holder = Label(root,text="").grid(row=2,column=0)
+	place_holder = Label(root,text="").grid(row=3,column=0)
 
 	#create sound group selection label and button
 	sound_group_selection = Label(root, text="Select sound group",bg="Light Blue")
@@ -172,50 +168,48 @@ if __name__ == "__main__":
 
 	sound_group = StringVar(root)
 	sound_group.set("Which sound group? ") # default value
-	w3 = OptionMenu(root, sound_group, "Practice Sound","Nature Sound Block 1","Nature Sound Block 2",
-										"Nature Sound Block 3", "Animal Sound Block 1","Animal Sound Block 2", 
-										"People Sound", "Transport Sound Block 1","Transport Sound Block 2",
-										command = get_sound_group)
+	w3 = OptionMenu(root, sound_group, "Practice Sound","Animal Sound","Nature Sound",
+										"People Sound", "Transport Sound", command = get_sound_group)
 	w3.grid(row=0, column=4, ipadx="30")
 
     #create sound play button and position
 	play_button = Button(root, text="Play Sound", fg="Black", bg="Light Blue", command = lambda: play())
-	play_button.grid(row=2, column=1,ipadx="40")
+	play_button.grid(row=4, column=1,ipadx="40")
 
 	#create Soundlabel and position
 	sound_label = Label (root, text= f'Play sound {idx + 1} of 30' , fg="Black", bg="Light Blue")
-	sound_label.grid(row=2, column=0)
-	place_holder2 = Label(root,text="").grid(row=3,column=0)
+	sound_label.grid(row=4, column=0)
+	place_holder2 = Label(root,text="").grid(row=5,column=0)
 	
 	#create an emotion dropdown button and position
 	emotion = StringVar(root)
 	emotion.set("Please select emotion experienced") # default value
 	w3 = OptionMenu(root, emotion, "Sadness", "Fear", "Happiness")
-	w3.grid(row=4, column=1, ipadx="30")
+	w3.grid(row=6, column=1, ipadx="30")
 
 	#Create emotion dropdown label
 	emotion_label = Label (root,text= f'Select emotion' , fg="Black", bg="Light Blue")
-	emotion_label.grid(row=4,column=0,)
-	place_holder3 = Label(root,text="").grid(row=5,column=0)
+	emotion_label.grid(row=6,column=0,)
+	place_holder3 = Label(root,text="").grid(row=7,column=0)
 
 	#create an emotion rating dropdown button and position
 	emotion_rating = StringVar(root)
 	emotion_rating.set("How intense was the emotion?") # default value
 	w4 = OptionMenu(root, emotion_rating, "1", "2", "3", "4", "5", "6", "7", "8", "9")
-	w4.grid(row=6, column=1, ipadx="30")
+	w4.grid(row=8, column=1, ipadx="30")
 
 	#create emotion rating dropdown label
 	emotion_rating_label = Label (root,text= 'Emotion Intensity' , fg="Black", bg="Light Blue")
-	emotion_rating_label.grid(row=6,column=0)
+	emotion_rating_label.grid(row=8,column=0)
 
 	# call excel function
 	excel()
 
 	# create a Submit Button and place into the root window
-	placeholder4 = Label(root, text="").grid(row=7,column=0)
-	placeholder5 = Label(root, text="").grid(row=8,column=0)
+	placeholder4 = Label(root, text="").grid(row=9,column=0)
+	placeholder5 = Label(root, text="").grid(row=10,column=0)
 	submit = Button(root, text="Submit", fg="Black",
 							bg="Red", command=insert)
-	submit.grid(row=9, column=2)
+	submit.grid(row=11, column=2)
 	# start the GUI
 	root.mainloop()
